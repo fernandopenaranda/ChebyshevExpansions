9653719681dbd1a49dc2eb5af7a799eec6b23ce4
Documenter.jl <documenter@juliadocs.github.io>
2022-02-19 12:14:19 +0000

    build based on a9f4e1e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChebyshevExpansions","category":"page"},{"location":"#ChebyshevExpansions","page":"Home","title":"ChebyshevExpansions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChebyshevExpansions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChebyshevExpansions]","category":"page"}]
+}
