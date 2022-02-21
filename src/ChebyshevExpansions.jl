95f32ce64c97fa8fd1aff9f8e2b43331fcdfcd11
fernandopenaranda <fernando.pennaranda@gmail>
2022-02-21 11:58:45 +0100

    .

--- src/ChebyshevExpansions.jl
+++ src/ChebyshevExpansions.jl
@@ -1,5 +1,9 @@
 module ChebyshevExpansions
-
-# Write your package code here.
-
+    # Use README as the docstring of the module:
+    @doc read(joinpath(dirname(@__DIR__), "README.md"), String) ChebyshevExpansions
+    using LinearAlgebra, SparseArrays
+    export KPMBuilder, momentaKPM
+    #export momentaKPM, densityKPM, dosKPM, averageKPM
+    include("KPM.jl")    
 end
+
