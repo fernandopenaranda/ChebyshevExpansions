c1fb2c2da2ab22a64a5c07b03663dc82e8798823
fernandopenaranda <fernando.pennaranda@gmail>
2022-02-21 19:11:47 +0100

    Exported functions

--- src/ChebyshevExpansions.jl
+++ src/ChebyshevExpansions.jl
@@ -2,7 +2,7 @@ module ChebyshevExpansions
     # Use README as the docstring of the module:
     @doc read(joinpath(dirname(@__DIR__), "README.md"), String) ChebyshevExpansions
     using LinearAlgebra, SparseArrays, ProgressMeter, FFTW, ArnoldiMethod
-    export KPMBuilder, randomkets, momentaKPM, densityKPM, dosKPM, averageKPM
+    export randomkets, momentaKPM, densityKPM, dosKPM, averageKPM
     #export momentaKPM, densityKPM, dosKPM, averageKPM
     include("KPM.jl")    
 end
